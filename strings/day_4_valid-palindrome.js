3ddc62bbf767bd05f7eb6179cbbc6ab85fddc08b
SulemJ <sulemjibrildawud@gmail.com>
2025-01-07 22:46:30 +0300

    Solved : day_4_valid-palindrome.js

---
+++ strings/day_4_valid-palindrome.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g,'');
+     let left = 0, right = s.length-1;
+     while(left < right ){
+         if(s[left] !== s[right]){
+             return false;
+         }
+         left++;
+         right--;
+     }
+     
+     return true;
+ };
